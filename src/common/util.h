16ac1d3a21332bd8ae89f2b0723902bdb34d251f
Benjamin Pearce <hawk.git@bcpearce.com>
2023-04-02 21:47:28 -0400

    fixed macro

--- src/common/util.h
+++ src/common/util.h
@@ -1,10 +1,11 @@
 #ifndef _UTIL_local_H
 #define _UTIL_local_H
 
+#include <sstream>
 #include <system_error>
 #include <string.h>
 
-#define SYSTEM_WRAPPER(FUNC, ...) SystemWrapper(FUNC, #FUNC, __VA_ARGS__)
+#define SYSTEM_WRAPPER(FUNC, ...) SystemWrapper(FUNC, #FUNC, ##__VA_ARGS__)
 
 template<class F, class... Ts>
 auto SystemWrapper(F func, const char* funcName, Ts... args) {
